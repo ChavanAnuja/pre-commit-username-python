752f71d1638753240ce6a2be619334d60f4cb84d
Anuja Chavan <chavanas1511@dnyanyog.org>
2024-04-07 00:06:50 +0530

    feat: Implement pre-commit check by username

---
+++ check_user_name.py
@@ -0,0 +1,14 @@
+import subprocess
+
+expectedUserName = "Anuja Chavan"
+
+userName = subprocess.run(["git", "config", "user.name"], capture_output=True, text=True).stdout.strip()
+
+if userName == expectedUserName:
+
+        print("You are using username as per configuration, proceeding to commit")
+        exit(0)
+
+else:
+        print(f"Configured username is not as per config. It should be {expectedUserName}")
+        exit(1)
